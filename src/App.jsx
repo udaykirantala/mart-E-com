b7840aad4f87b8eb90aa7ec9f0dcd5a870eaf243
udaykirantala <udaykirantala@gmail.com>
2025-05-23 14:31:04 +0530

    Installing All Laibarys

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Carousel } from './Componets/Carousel'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Carousel/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Componets/Carousel.jsx
@@ -0,0 +1,19 @@
+export const Carousel=()=>{
+    return(
+        <>
+        <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="https://images.pexels.com/photos/32138216/pexels-photo-32138216/free-photo-of-cat-by-vibrant-doorway-in-artistic-alley.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="https://images.pexels.com/photos/27580823/pexels-photo-27580823/free-photo-of-woman-in-red-shirt-posing-with-hands-in-hair.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="https://images.pexels.com/photos/27580823/pexels-photo-27580823/free-photo-of-woman-in-red-shirt-posing-with-hands-in-hair.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" className="d-block w-100" alt="..."/>
+    </div>
+  </div>
+</div>
+        </>
+    )
+}
